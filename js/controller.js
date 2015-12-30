be9c2ec387d4197223bae1a26b70be907a373e76
oscarvillango <mengo.villafuerte@gmail.com>
2015-12-30 09:27:06 -0600

    Two way binding and ngModel

--- js/controller.js
+++ js/controller.js
@@ -3,3 +3,26 @@ var app = angular.module("MyFirstApp", []);
 app.controller("firstController", function($scope){
 	$scope.nombre = "Oscar";
 });
+
+
+var app = angular.module("NgModelApp", []);
+
+app.controller("ngmFirstController", function($scope){
+	$scope.nombre = "Oscar";
+	$scope.nuevoComentario = {};
+	$scope.comentarios = [
+		{
+			comentario : "Primer comentario",
+			usuario : "User 1"
+		},
+		{
+			comentario : "Segundo comentario",
+			usuario : "User 2"
+		}
+	];
+
+	$scope.agregarComentario = function(){
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario = {};
+	}
+});
